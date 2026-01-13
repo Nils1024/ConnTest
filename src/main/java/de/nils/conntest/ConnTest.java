f01a171e581b75773be88774c1f0e40f99c34a25
Nils1024 <78668364+Suchti18@users.noreply.github.com>
2026-01-13 09:19:26 +0100

    Renamed UncaughtHandler to UncaughtExceptionHandler

--- src/main/java/de/nils/conntest/ConnTest.java
+++ src/main/java/de/nils/conntest/ConnTest.java
@@ -1,6 +1,6 @@
 package de.nils.conntest;
 
-import de.nils.conntest.exception.UncaughtHandler;
+import de.nils.conntest.exception.UncaughtExceptionHandler;
 import de.nils.conntest.gui.UIStart;
 import de.nils.conntest.model.Model;
 import de.nils.conntest.model.event.EventQueue;
@@ -9,7 +9,7 @@ public class ConnTest
 {
     public static void main(String[] args)
     {
-        Thread.setDefaultUncaughtExceptionHandler(new UncaughtHandler());
+        Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler());
 
         Model.getModel();
         new UIStart();
--- src/main/java/de/nils/conntest/exception/UncaughtExceptionHandler.java
+++ src/main/java/de/nils/conntest/exception/UncaughtExceptionHandler.java
@@ -2,11 +2,10 @@ package de.nils.conntest.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.lang.Thread.UncaughtExceptionHandler;
 
-public class UncaughtHandler implements UncaughtExceptionHandler
+public class UncaughtExceptionHandler implements Thread.UncaughtExceptionHandler
 {
-	private static final Logger log = LoggerFactory.getLogger(UncaughtHandler.class);
+	private static final Logger log = LoggerFactory.getLogger(UncaughtExceptionHandler.class);
 	
 	@Override
 	public void uncaughtException(Thread t, Throwable e)
